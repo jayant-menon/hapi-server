0ba7bcb55a1da20ad9b25e912f6bd6f4f7b8a970
Jayant Menon <jayant.menon@launchventures.co>
2022-10-12 17:12:18 +0530

    added more routes

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const init = async () => {
   // contains details about the request
   // contains request.auth, request.query, request.path, params, etc.
   // h is a response toolkit which we can use to do things (like redirect the user, etc.)
+  // h provides both utilities for manipulating responses as well as other information
   server.route({
     method: "GET",
     path: "/",
@@ -36,18 +37,66 @@ const init = async () => {
   // each key is a path parameter name
   // so if i go to /users/jayant
   // request.params = { user: "jayant"}
+  // The questionmark in the parameter sets it as optional
+  // whatever i have named the query param in the path, is what i
+  // have to access in the request.params object
   server.route({
     method: "GET",
-    path: "/users/{user}",
+    path: "/users/{user?}",
     handler: (request, h) => {
-      return `<h1>Hello ${request.params.user}</h1>`;
+      if (request.params.user) {
+        return `<h1>Hello ${request.params.user}</h1>`;
+      }
+      return `<h1>Hello Stranger!</h1>`;
+    },
+  });
+
+  // Handling query params - query params are key/value pairs that
+  // go in the URL and are seperated by ampersands
+  // Eg: http://localhost:3000/greeting?name=jayant&lastname=menon
+  server.route({
+    method: "GET",
+    path: "/greeting",
+    handler: (request, h) => {
+      const nameExists = request.query.name;
+      const lastnameExists = request.query.lastname;
+
+      if (nameExists && lastnameExists) {
+        return `<h1>Hello ${request.query.name} ${request.query.lastname}</h1>`;
+      }
+
+      return `<h1>Hello unspecified person</h1>`;
+    },
+  });
+
+  // redirection using h
+  server.route({
+    method: "GET",
+    path: "/redirect",
+    handler: (request, h) => {
+      return h.redirect("/");
+    },
+  });
+
+  // wildcard - 404
+  // it doesn't have to be the word any, it can be another word
+  // the convention is to use {any*} but we could do {hello*}
+  // Basically, we are saying any path that matches root '/'
+  // and beyond and isn't found will routed and handled here
+  // Any path the user searches for that doesn't exist and starts with '/'
+  // will be handled here
+  server.route({
+    method: "GET",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return `<h1>Page not found</h1>`;
     },
   });
 
   await server.start();
   // logging the fact the server started, server.info.uri refers to the passed in param to the server object,
   // in this case http://localhost:3000
-  console.log(`Server started on {server.info.uri}`);
+  console.log(`Server started on ${server.info.uri}`);
 };
 
 // The process object is a global object that provides information about the current node process
