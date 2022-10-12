0b4391b391e5a641508706f906f381c1a1767891
Jayant Menon <jayant.menon@launchventures.co>
2022-10-12 14:36:33 +0530

    commented the server for my understanding

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
-const { configureRoutes } = require("./routes/routes");
 
-const server = Hapi.server({
-  port: 3000,
-  host: "localhost",
-});
-
+// creating a server object
+// a common practice with hapi is to wrap the server object in an async function
+// to be able to use the await keyword
+// Note: an async function always returns a promise object
 const init = async () => {
+  // The hapi server accepts an object as a param, the most imp keys in the
+  // param object are 'host' and the 'port'
+  const server = Hapi.Server({
+    host: "localhost",
+    port: 3000,
+  });
+
+  // setting the home route
+  // server.route() accepts an object as a param, the object accepts at least
+  // a HTTP method, a path, and a handler
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return `<h1>Hello World!</h1>`;
+    },
+  });
+
   await server.start();
-  await configureRoutes(server);
-  console.log("Server running on %s", server.info.uri);
+  // logging the fact the server started, server.info.uri refers to the passed in param to the server object,
+  // in this case http://localhost:3000
+  console.log(`Server started on {server.info.uri}`);
 };
 
-// server.route({
-//   method: "GET",
-//   path: "/home",
-//   handler: (request, h) => {
-//     return "<h1>Hello Jayant!</h1>";
-//   },
-// });
-
-// server.route({
-//   method: "GET",
-//   path: "/home/{name}",
-//   handler: (request, h) => {
-//     return `Hello ${request.params.name}!`;
-//   },
-// });
+// The process object is a global object that provides information about the current node process
+// that's running
+// The 'unhandledRejection' event is emitted when a promise is rejected and no error handler is
+// attached to the promise within a turn of the event loop
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
 
+// Since our init function is async, it will return a promise.
+// So we need to handle the case in which the promise is rejected
+// that we're doing above - instead of doing something like `init().catch()`
+
+// starting the server
 init();
